knowitcz/copilot-training-ccon-01

Task-3: Implement online banking features with transfer functionality

app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.db import SQLModel, engine
 from app.api.account_routes import router as account_router
+from app.api.bank_routes import router as bank_router
 from app.startup import create_default_accounts
 
 SQLModel.metadata.create_all(bind=engine)
@@ -8,5 +9,6 @@ SQLModel.metadata.create_all(bind=engine)
 app = FastAPI()
 
 app.include_router(account_router, prefix="/api/v1", tags=["v1"])
+app.include_router(bank_router, prefix="/api/v0", tags=["v0"])
 
 create_default_accounts()

knowitcz/copilot-training-10-16

app/main.py
@@ -3,6 +3,7 @@ from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 from app.db import SQLModel, engine
 from app.api.account_routes import router as account_router
+from app.api.bank_routes import router as bank_router
 from app.startup import create_default_accounts
 
 SQLModel.metadata.create_all(bind=engine)
@@ -15,5 +16,6 @@ def serve_static_files():
     return FileResponse("resources/static/index.html")
 
 app.include_router(account_router, prefix="/api/v1", tags=["account"])
+app.include_router(bank_router, prefix="/api/v1", tags=["bank"])
 
 create_default_accounts()